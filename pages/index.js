d46c1914bac8ce9d11e9c454bae3467251b8d625
internalapp-nptel-ac-in <mrthunder369@gmail.com>
2023-05-17 17:25:41 +0530

    Fix responsivenes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
       <section className="container w-[70%] mx-auto p-4 flex  flex-col items-center bg-white rounded-br-md rounded-bl-md min-h-[20vh] drop-shadow-lg">
         <Image src={logo} width={500} height={500} layout="responsive" />
-        <h1 className="font-sans text-[#073E6C] text-3xl mt-5">
+        <h1 className="font-sans text-[#073E6C] md:text-3xl xm:text-lg mt-5 text-center">
           Jan-Apr 2023 NPTEL Exams
         </h1>
         <Link href="/login" className="mt-10 text-xl text-blue-600">
